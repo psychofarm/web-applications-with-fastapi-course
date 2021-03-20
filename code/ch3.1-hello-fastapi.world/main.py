bce2aeb7388a63c50af9790fd5e5aed51dce80e7
Frank J. Genova <frank.j.genova@gmail.com>
2021-03-20 14:10:42 -0400

    simple hello world web app

--- code/ch3.1-hello-fastapi.world/main.py
+++ code/ch3.1-hello-fastapi.world/main.py
@@ -1,1 +1,16 @@
-print("Hello, I am HAL.")
+import fastapi
+import uvicorn
+from uvicorn.main import main
+
+app = fastapi.FastAPI()
+
+
+@app.get('/')
+def index():
+    return {'message': "Hello x World"}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
+else:
+    pass
